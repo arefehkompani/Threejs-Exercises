b5a9a5400fd1f078ec6252dfcf559b780c03c048
Arefeh Kompani <awriik00@gmail.com>
2022-09-02 19:14:05 +0430

    Working on Transformation

--- basic-scene/src/script.js
+++ basic-scene/src/script.js
@@ -1,14 +1,56 @@
 import './style.css'
 import * as THREE from 'three'
 
+// Canvas 
+const canvas = document.querySelector('.webgl')
+
 // Scene
 const scene = new THREE.Scene();
 
-// Red cube
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({color: 'red'})
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh);
+
+/**
+ * Objects
+ */
+const group = new THREE.Group()
+group.position.y = 1
+group.scale.y = 2
+group.rotation.y = 1
+scene.add(group)
+
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({color: 'red'}),
+)
+cube1.position.x = -2
+group.add(cube1)
+
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({color: 'green'}),
+)
+group.add(cube2)
+
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({color: 'blue'}),
+)
+cube3.position.x = 2
+group.add(cube3)
+
+
+
+// Rotation
+
+
+// Scale 
+// mesh.scale.x = 2
+// mesh.scale.y = 0.5
+// mesh.scale.z = 0.5
+
+
+// Axes Helpers
+const axesHelper = new THREE.AxisHelper();
+scene.add(axesHelper);
 
 // Camera - FOV=fields of view
 const sizes = {
@@ -17,12 +59,12 @@ const sizes = {
 }
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
-camera.position.x = 0.1
-camera.position.y = 1
+// camera.position.x = 0.1
+// camera.position.y = 1
 scene.add(camera);
 
+
 // Renderer
-const canvas = document.querySelector('.webgl')
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas,
 })
